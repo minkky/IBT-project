8c2f4788a7af46fc2324a6c6004a946c354c403c
SM-DESKTOP@MINJI <minjiseo95@gmail.com>
2019-02-01 14:24:17 +0900

    numberOfAtom, numberOfBonds 각각 한 줄씩 한 셀에 들어갈 수 있도록 수정

--- read.py
+++ read.py
@@ -3,12 +3,15 @@ import os, csv
 csv_file = open("zinc.csv", "w", encoding="utf-8", newline="\n")
 writer = csv.writer(csv_file)
 
+counts_line = ["numberOfAtoms", "numberOfBonds"]
+
 for root, dirs, files in os.walk('.'):
 	for fname in files:
 		full_fname = os.path.join(root, fname)
 		if ".sdf" in full_fname and ".gz" not in full_fname:
 			print(full_fname)
 			read_file = open(full_fname, 'r')
+			line_num = 0
 			while True:
 				line = read_file.readline()
 				
@@ -16,11 +19,25 @@ for root, dirs, files in os.walk('.'):
 					break
 
 				if "ZINC" in line:
+					line_num = 0
+					numberOfAtoms = 0
+					numberOfBonds = 0
 					new_line = True
 					compound = []
-				
+
+				line_num = line_num + 1
 				line = " ".join(line.split())
 
+				if line_num == 4:
+					numberOfAtoms = int(line.split(" ")[0])
+					numberOfBonds = int(line.split(" ")[1])
+					
+				'''if line_num == int(numberOfAtoms) + 4:
+					print(line)
+
+				if line_num == numberOfAtoms + numberOfBonds + 4:
+					print(line)
+'''
 				if "END" in line or "$$$$" in line:
 					new_line = False
 					if len(compound) == 0:
@@ -29,7 +46,25 @@ for root, dirs, files in os.walk('.'):
 					del compound[:]
 
 				if new_line:
-					compound.extend(line.split(" "))
+
+					if line_num > 4 and line_num <= numberOfAtoms + 4:
+						line_split = line.split(" ")
+						position_atom = line_split[:4]
+						extras = [str(','.join(line_split[4:]))]
+						line_split = [position_atom + extras]
+						#print(line_split, ','.join(line_split))
+						# line_split = position_atom + extras --> extras 만 한 컬럼에 같이 들어감
+
+					elif line_num > numberOfAtoms + 4 and line_num <= numberOfBonds + numberOfAtoms + 4:
+						line_split = line.split(" ")
+						relation_atom = line_split[:3]
+						extras = [','.join(line_split[3:])]
+						line_split = [relation_atom + extras]
+
+					else:
+						line_split = line.split(" ")
+
+					compound.extend(line_split)
 
 
 
