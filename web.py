832a55ea42d9fcdab21226c2c88ec12124480143
SM-DESKTOP@MINJI <minjiseo95@gmail.com>
2019-02-13 17:23:14 +0900

    condition modified

--- web.py
+++ web.py
@@ -44,22 +44,49 @@ def getRangeLetter(letter):
 			start = start + 1
 	return letters
 
+def printMenuWithMeaning(alpha, meaning):
+	printDash()
+	for index, mean in zip(alpha, meaning):
+		print("\t" + index, ":", mean)
+	printDash()
+
 fileURL = "http://files.docking.org/3D/"
 
 printMolecularMenu()
 molecular_weight = input("Molecular Weight(ex, A or C~F) : ") # need input molecular except code
 molecular_weight = molecular_weight.upper()
 printLogPMenu()
-logP = input("logP(ex, A or C~F) : ")
-logP = logP.upper()
+logP = input("logP(ex, A or C~F) : ").upper()
 print()
 
 firstLetter = getRangeLetter(molecular_weight)
 secondLetter = getRangeLetter(logP)
-thirdLetter = ["A", "B", "C", "E", "G", "I"]
-fourthLetter = ["A", "B", "C", "D", "E", "F"]
-fifthLetter = ["H", "L", "M", "R"]
-sixthLetter = ["L", "M", "N", "O", "P"]
+thirdLetter = ["A", "B", "C", "E", "G", "I"] # reactivity - E is standard(default)
+fourthLetter = ["A", "B", "C", "D", "E", "F"] #  purchasability - D is Wait OK(default)
+fifthLetter = ["H", "L", "M", "R"] # pH range
+sixthLetter = ["L", "M", "N", "O", "P"] # net molecular charge
+thirdMeaning = ["anodyne", "Bother", "clean", "mild reactivity", "reactivity ok", "hot chemistry ok"]
+fourthMeaning = ["in stock", "in stock", "in stock via agent", "make on demand", "boutique", "annotated"]
+fifthMeaning = ["ref (7.4)", "mid (near 7.4)", "low (around 6.4)", "high (around 8.4)"]
+sixthMeaning = ["neutral", "minus", "minus 2", "plus 1", "plus 2"]
+
+
+printMenuWithMeaning(thirdLetter, thirdMeaning)
+third = input("reactivity(E is default) : ").upper()
+thirdLetter = thirdLetter[:thirdLetter.index(third)+1]
+
+printMenuWithMeaning(fourthLetter, fourthMeaning)
+fourth = input("purchasability(D is default) : ").upper()
+fourthLetter = fourthLetter[:fourthLetter.index(fourth)+1]
+
+printMenuWithMeaning(fifthLetter, fifthMeaning)
+fifth = input("pH range(R, M is default) : ").upper()
+fifthLetter = [x.strip() for x in fifth.split(",")]
+
+printMenuWithMeaning(sixthLetter, sixthMeaning)
+sixth = input("net molecular charge(default is All) : ").upper()
+if sixth != "ALL":
+	sixthLetter = [x.strip() for x in sixth.split(",")]
 
 substances = []
 subsets = []
